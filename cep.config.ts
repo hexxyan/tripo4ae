bf11919bec5a058b364c2e6cb1dc686f4a6f5a16
hexxyan <hexxyan@users.noreply.github.com>
2026-06-10 11:01:50 +0800

    feat: initialize Bolt CEP project for Tripo4AE
    - Scaffold Bolt CEP React template with create-bolt-cep
    - Configure cep.config.ts: AEFT host [24.0,99.9], CSXS 9.0
    - Panel: 320x600px, min 280x400px
    - CEF params: --allow-file-access-from-files, --allow-file-access
    - Create CSXS/manifest.xml with extension specs
    - Download CSInterface.js v12.0.0 from Adobe CEP-Resources
    - Install: zustand, jest, ts-jest, @types/jest,
      @testing-library/react, @testing-library/jest-dom
    - Dev server verified working at localhost:3000/main/
    - Build verified: manifest + symlink + debug file generated
    - Preserve existing research .md files

---
+++ cep.config.ts
@@ -0,0 +1,66 @@
+import { CEP_Config } from "vite-cep-plugin";
+import { version } from "./package.json";
+
+const config: CEP_Config = {
+  version,
+  id: "com.tripo4ae.panel",
+  displayName: "Tripo4AE",
+  symlink: "local",
+  port: 3000,
+  servePort: 5000,
+  startingDebugPort: 8860,
+  extensionManifestVersion: 6.0,
+  requiredRuntimeVersion: 9.0,
+  hosts: [
+    { name: "AEFT", version: "[24.0,99.9]" },
+  ],
+
+  type: "Panel",
+  iconDarkNormal: "./src/assets/light-icon.png",
+  iconNormal: "./src/assets/dark-icon.png",
+  iconDarkNormalRollOver: "./src/assets/light-icon.png",
+  iconNormalRollOver: "./src/assets/dark-icon.png",
+  parameters: [
+    "--v=0",
+    "--enable-nodejs",
+    "--mixed-context",
+    "--allow-file-access-from-files",
+    "--allow-file-access",
+  ],
+  width: 320,
+  height: 600,
+
+  panels: [
+    {
+      mainPath: "./main/index.html",
+      name: "main",
+      panelDisplayName: "Tripo4AE",
+      autoVisible: true,
+      width: 320,
+      height: 600,
+      minWidth: 280,
+      minHeight: 400,
+    },
+  ],
+  build: {
+    jsxBin: "off",
+    sourceMap: true,
+  },
+  zxp: {
+    country: "US",
+    province: "CA",
+    org: "Company",
+    password: "password",
+    tsa: [
+      "http://timestamp.digicert.com/", // Windows Only
+      "http://timestamp.apple.com/ts01", // MacOS Only
+    ],
+    allowSkipTSA: false,
+    sourceMap: false,
+    jsxBin: "off",
+  },
+  installModules: [],
+  copyAssets: [],
+  copyZipAssets: [],
+};
+export default config;
